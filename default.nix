530e18a6511d473f14553f7c888ad8373d97f2c4
momeemt <momiimt@gmail.com>
2023-09-12 14:38:03 +0900

    fix: nix-build including do modules

--- default.nix
+++ default.nix
@@ -1,9 +1,19 @@
-{ pkgs ? import ./nix/pkgs.nix }:
-pkgs.buildGoModule {
+{ pkgs ? (
+    let
+      sources = import ./nix/sources.nix;
+    in
+    import sources.nixpkgs {
+      overlays = [
+        (import "${sources.gomod2nix}/overlay.nix")
+      ];
+    }
+  )
+}:
+
+pkgs.buildGoApplication {
   pname = "coco-backend";
   version = "0.1.0";
-
+  pwd = ./.;
   src = ./.;
-
-  vendorHash = null;
+  modules = ./gomod2nix.toml;
 }
