d201717e5dfd715c92e3ae436ee03390a404111f
momeemt <momiimt@gmail.com>
2023-09-12 14:37:49 +0900

    add: niv and gomod2nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,22 @@
-{ pkgs ? import ./nix/pkgs.nix }:
+{ pkgs ? (
+    let
+      sources = import ./nix/sources.nix;
+    in
+    import sources.nixpkgs {
+      overlays = [
+        (import "${sources.gomod2nix}/overlay.nix")
+      ];
+    }
+  )
+}:
+let
+  goEnv = pkgs.mkGoEnv { pwd = ./.; };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    goEnv
+    niv
+    gomod2nix
     go
     gotools
     go-tools
